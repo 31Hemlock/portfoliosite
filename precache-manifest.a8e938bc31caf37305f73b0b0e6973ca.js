331c325c3e45ac7f2403793197937647406acb43
31Hemlock <58008724+31Hemlock@users.noreply.github.com>
2020-06-18 14:38:53 -0400

    Updates

---
+++ precache-manifest.a8e938bc31caf37305f73b0b0e6973ca.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dc14482efe321cdc0369d3c10999888",
+    "url": "/portfoliosite/index.html"
+  },
+  {
+    "revision": "68af3ebb82b0d29811c4",
+    "url": "/portfoliosite/static/css/main.b189dde0.chunk.css"
+  },
+  {
+    "revision": "fa29b0a8cbd0c2792ece",
+    "url": "/portfoliosite/static/js/2.a5e838b7.chunk.js"
+  },
+  {
+    "revision": "7eac5df8fd17c53ea42b25fbf2237fb3",
+    "url": "/portfoliosite/static/js/2.a5e838b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68af3ebb82b0d29811c4",
+    "url": "/portfoliosite/static/js/main.2a9d2751.chunk.js"
+  },
+  {
+    "revision": "6cdf31383e7e4ab6f269",
+    "url": "/portfoliosite/static/js/runtime-main.fa514bd4.js"
+  },
+  {
+    "revision": "642f946a1dc86cd5391ac6791c9521e9",
+    "url": "/portfoliosite/static/media/background.642f946a.png"
+  },
+  {
+    "revision": "7900da91f535439c7951b7835f8ae6cb",
+    "url": "/portfoliosite/static/media/chess_analytics.7900da91.mp4"
+  },
+  {
+    "revision": "470f114609c723204a45101a6f602eaf",
+    "url": "/portfoliosite/static/media/excel.470f1146.png"
+  },
+  {
+    "revision": "2a0fb74e85052a1a118862c7671136b0",
+    "url": "/portfoliosite/static/media/flow.2a0fb74e.mp4"
+  },
+  {
+    "revision": "39c9a01855c2e22f21953c3152a5e8df",
+    "url": "/portfoliosite/static/media/machine_learning.39c9a018.png"
+  },
+  {
+    "revision": "0b7ab77954e730827f68ef8e6a47fe98",
+    "url": "/portfoliosite/static/media/powershell.0b7ab779.mp4"
+  },
+  {
+    "revision": "b0f1fa1cc9bdcd22300c7015c0d12d56",
+    "url": "/portfoliosite/static/media/py_data_manipulation.b0f1fa1c.png"
+  },
+  {
+    "revision": "0d838958eb4e5d67ccd04b7cf0e08658",
+    "url": "/portfoliosite/static/media/sql.0d838958.png"
+  },
+  {
+    "revision": "67a80fff4431756775a9ef13c00f6f17",
+    "url": "/portfoliosite/static/media/threeBars.67a80fff.svg"
+  },
+  {
+    "revision": "aa5f97a2b7f1dfe0076ba5f3568133fc",
+    "url": "/portfoliosite/static/media/visual_studio.aa5f97a2.mp4"
+  },
+  {
+    "revision": "7095eeb3da1d11317a2ed2c8f299930f",
+    "url": "/portfoliosite/static/media/webcommerce_project.7095eeb3.mp4"
+  }
+]);
